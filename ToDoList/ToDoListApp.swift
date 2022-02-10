e9a37180bc69ce17f8071e138d15a520c81ba0be
user <user@users-MacBook-Pro.local>
2022-02-10 13:14:14 +0200

    Initial commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by user on 10.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
